84350c06a906a087a90a181a55c8530fc3436638
Katiusca Henriquez <katiuscahenriquez75@gmail.com>
2023-07-21 14:59:15 -0500

    Realizo una actualizacion del codigo donde utilizo el nivel 6 de seguridad ya utenticacion aprendido. Ademas utilizo la coneccion y autenticacion con google.

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 //jshint esversion:6
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-///1-instalamos en hyper y requerimos
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
-
-const uri = "mongodb://127.0.0.1:27017/usuarioDB";
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
-
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-//2-conectamos la base de datos
+app.use
+(session({
+  secret:"Nuestro pequeño secreto",
+  resave: false,
+  saveUninitialized: false,
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//conexion a la base de datos
+const uri = "mongodb://127.0.0.1:27017/usuarioDB";
 mongoose.connect(uri)
   .then(() => {
     console.log("conexion exitosa");
@@ -23,76 +38,155 @@ mongoose.connect(uri)
   .catch((err) => {})
 ///
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-///3-creamos un nuevo esquema para la base de datos
-
 const usuarioSchema = new mongoose.Schema({
   correo: String,
-  contraseña: String
+  contraseña: String,
+  googleId: String,
+  secret: String
 });
 
-usuarioSchema.plugin(encrypt, { secret: process.env.SECRETO, encryptedFields: ["contraseña"] });
-///4- usamos el schema anterior para configurar el mongoose model y especificamos en "el nombre de la coleccion" en singular
+usuarioSchema.plugin(passportLocalMongoose);
+usuarioSchema.plugin(findOrCreate);
 
 const Usuario =  mongoose.model("Usuario", usuarioSchema);
 
-//////
+passport.use(Usuario.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Usuario.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function(req, res){
 res.render("home");
 });
 
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", function(req, res){
 res.render("login");
 });
 
-
 app.get("/register", function(req, res){
 res.render("register");
 });
 
-//// recuerda utilizar promesa pra que funcione ya que la explicacion de angela yu esta desactualizada////
-//Utiliza o .then o try para que pueda funcionar, segun la documentacion////
-/// se debe usar await, por lo que por mi logica lo mejor seria utilizar try ya que este utilza await////
- //5- crea un nuevo usuario y envia a la pag de secretos si la persona se registra
+app.get("/secrets", function (req, res) {
+Usuario.find({"secret" : {$ne:null}})
+.then((encontrarUs)=> {
+  if (encontrarUs) {
+    res.render("secrets", {usurioJuntoSuSecret: encontrarUs });
+  }
+})
+. catch((err) => {
+  console.log(err);
+});
+});
 
-app.post("/register",async function(req,res){
-  const newUsuario = new Usuario ({
-    correo: req.body.usuario,
-    contraseña: req.body.contraseña
-  });
-  try {
-    await newUsuario.save();
-    res.render("secrets");
-  } catch (error) {
-    console.log(error);
+app.get("/submit", function(req,res){
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
   }
 });
-//5- Aqui validaremos si la persona si se registro efectiamente
-app.post("/login",async(req,res)=>{
-       const usuario = req.body.usuario;
-       const contraseña = req.body.contraseña;
+app.post("/submit", function (req, res) {
+    const secretosGuardados = req.body.secret;
+    Usuario.findById(req.user.id)
+      .then((encontrarUs) => {
+          if (encontrarUs) {
+            encontrarUs.secret = secretosGuardados;
+            encontrarUs.save()
+      .then(() => {
+res.redirect("/secrets");
+    });
+} else {
+  console.log("User not found");
+    }
+  })
+.catch((err) => {
+console.log(err);
+  });
+    });
+app.get("/logout", function(req,res){
+req.logout(function(err){
+  if (err) {
+    return next(err);
+  } else {
+    res.redirect("/");
+  }
+})
 
-       try {
-           const encontrarU = await Usuario.findOne({correo:usuario})
-           if(encontrarU){
-               if(encontrarU.contraseña===contraseña){
-                   res.render('secrets');
-               }else{
-                   res.send('<h1> Contraseña invalida...</h1>');
-               }
-           }else{
-               res.send("<h1>Usuario no encontrado...</h1>");
-           }
-       } catch (err) {
-           console.log(err);
-       }
-   });
+});
+
+app.post("/register", function (req, res) {
+  Usuario.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login",async(req,res)=>{
+const user = new Usuario ({
+  username: req.body.username,
+  password: req.body.password
+});
+
+req.login(user, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/secrets");
+    });
+  }
+});
+});
 
 
 
